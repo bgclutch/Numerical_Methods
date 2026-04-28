95539d554f9b0c12a74b07ef5c3a6b57fa8d1323
bglutch <bvolsky2006@gmail.com>
2026-04-28 18:43:48 +0300

    code formatted

--- Benchmark_task/inc/benchlib.hpp
+++ Benchmark_task/inc/benchlib.hpp
@@ -1,33 +1,38 @@
 #pragma once
 
 #include <immintrin.h>
+
+#include <bit>
 #include <cstdint>
 #include <fstream>
 #include <iostream>
-#include <vector>
-#include <bit>
 #include <numeric>
+#include <vector>
 
-namespace benchlib {
+namespace benchlib
+{
 constexpr int ITERATIONS = 10000;
 
-template<typename Func, typename ElemType>
-void funcLatencyTest(Func&& testFunc, const std::vector<ElemType>& data, std::ostream& output = std::cerr) {
-    using bitType = std::conditional_t<sizeof(ElemType) == 8, uint64_t, uint32_t>;
+template <typename Func, typename ElemType>
+void funcLatencyTest(Func&& testFunc, const std::vector<ElemType>& data, std::ostream& output = std::cerr)
+{
+    using bitType          = std::conditional_t<sizeof(ElemType) == 8, uint64_t, uint32_t>;
     constexpr bitType ZERO = static_cast<bitType>(0ull);
 
-    ElemType offset = std::bit_cast<ElemType>(ZERO);
+    ElemType offset            = std::bit_cast<ElemType>(ZERO);
     volatile ElemType zeroMask = std::bit_cast<ElemType>(ZERO);
-    auto size = data.size();
+    auto size                  = data.size();
     std::vector<ElemType> res(size);
     uint64_t result = UINT64_MAX;
 
-    for (int n = 0; n != ITERATIONS; ++n) {
+    for (int n = 0; n != ITERATIONS; ++n)
+    {
         _mm_lfence();
         uint64_t begin = __rdtsc();
         _mm_lfence();
 
-        for (size_t i = 0; i != size; ++i) {
+        for (size_t i = 0; i != size; ++i)
+        {
             res[i] = std::forward<Func>(testFunc)(data[i] + offset);
 
             bitType bits = std::bit_cast<bitType>(res[i]);
@@ -45,18 +50,21 @@ void funcLatencyTest(Func&& testFunc, const std::vector<ElemType>& data, std::os
     output << "Latency: " << cpe << " CPE" << std::endl;
 }
 
-template<typename Func, typename ElemType>
-void funcThroughputTest(Func&& testFunc, const std::vector<ElemType>& data, std::ostream& output = std::cerr) {
+template <typename Func, typename ElemType>
+void funcThroughputTest(Func&& testFunc, const std::vector<ElemType>& data, std::ostream& output = std::cerr)
+{
     auto size = data.size();
     std::vector<ElemType> res(size);
     uint64_t result = UINT64_MAX;
 
-    for (int n = 0; n != ITERATIONS; ++n) {
+    for (int n = 0; n != ITERATIONS; ++n)
+    {
         _mm_lfence();
         uint64_t begin = __rdtsc();
         _mm_lfence();
 
-        for (size_t i = 0; i != size; ++i) {
+        for (size_t i = 0; i != size; ++i)
+        {
             res[i] = std::forward<Func>(testFunc)(data[i]);
         }
 
@@ -69,4 +77,4 @@ void funcThroughputTest(Func&& testFunc, const std::vector<ElemType>& data, std:
     double cpe = static_cast<double>(result) / size;
     output << "Throughput: " << cpe << " CPE" << std::endl;
 }
-} // namespace benchlib
+}  // namespace benchlib
--- Benchmark_task/src/main.cpp
+++ Benchmark_task/src/main.cpp
@@ -1,26 +1,31 @@
-#include "benchlib.hpp"
-#include <iostream>
-#include <fstream>
+#include <algorithm>
 #include <cmath>
+#include <fstream>
+#include <iostream>
+#include <random>
 #include <vector>
 
-int main() {
-    std::vector<double> data = {0.1, 0.00001, 1, 2, 100};
+#include "benchlib.hpp"
 
-    auto stdlog = [](double x) {
-        return std::log(x);
-    };
+int main()
+{
+    constexpr size_t size = 10000;
+    std::vector<float> data(size);
+    std::mt19937 gen(42);
 
-    auto stdexp = [](double x){
-        return std::exp(x);
-    };
+    std::generate(data.begin(), data.end(), gen);
+    auto stdlog = [](double x) { return std::log(x); };
 
-    std::cout << "stdlog res: " << "\n";
+    auto stdexp = [](double x) { return std::exp(x); };
+
+    std::cout << "stdlog res: "
+              << "\n";
     benchlib::funcLatencyTest(stdlog, data);
     benchlib::funcThroughputTest(stdlog, data);
     std::cout << "-------------------------------\n";
 
-    std::cout << "stdexp res: " << "\n";
+    std::cout << "stdexp res: "
+              << "\n";
     benchlib::funcLatencyTest(stdexp, data);
     benchlib::funcThroughputTest(stdexp, data);
     std::cout << "-------------------------------\n";
